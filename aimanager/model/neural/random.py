027f1928cb9215216342c562167cf97dc1b4ffe5
levin.brinkmann01 <brinkmann@mpib-berlin.mpg.de>
2022-03-18 20:27:29 +0100

    Add visualisation of data collection.

--- aimanager/model/neural/random.py
+++ aimanager/model/neural/random.py
@@ -6,12 +6,22 @@ class RandomArtificialHumans(th.nn.Module):
         super(RandomArtificialHumans, self).__init__()
         self.max_contribution = max_contribution
         self.device = device
-    
-    def forward(self, view):
+
+    def forward(self, punishments, **kwargs):
         """
             view: batch (b), round (r), agents (a), inputs (i)
         """
-        n_batch, n_rounds, n_agents, n_inputs  = view.shape 
+        n_batch, n_rounds, n_agents = punishments.shape
 
-        q = th.rand((n_batch, n_rounds, n_agents, self.max_contribution), device=self.device)
+        q = th.rand((n_batch, n_rounds, n_agents,
+                    self.max_contribution), device=self.device)
         return q
+
+    def act(self, **view):
+        view = {
+            k: v.unsqueeze(0).unsqueeze(0)
+            for k, v in view.items()
+        }
+        q = self(**view)
+        q = q[0, 0]
+        return q.argmax(dim=1)
